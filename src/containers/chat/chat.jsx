cb383eac398c3a095748cec4e70d5d03366aaf6f
zxfjd3g <258147149@qq.com>
2018-09-21 16:39:52 +0800

    msg

--- src/containers/chat/chat.jsx
+++ src/containers/chat/chat.jsx
@@ -24,35 +24,62 @@ class Chat extends Component {
     const to = this.props.match.params.userid
 
     this.props.sendMsg({content, from, to})
+
+    this.setState({
+      content: ''
+    })
   }
 
   render () {
+
+    const {user, chat} = this.props
+    const {users, chatMsgs} = chat
+
+
+
+    // 得到当前用户的id
+    const meId = user._id
+    // 得到目标用户的id
+    const targetId = this.props.match.params.userid
+    // 得到当前聊天的chatId
+    const chatId = [meId, targetId].sort().join('-')
+
+    // 得到当前登陆用户与目标用户的所有msg组件的数组
+    const msgs = chatMsgs.filter(msg => msg.chat_id=chatId)
+
+    // 得到目标用户
+    const targetUser = users[targetId]
+    const targetHeader = require(`../../assets/images/${targetUser.header}.png`)
+
     return (
       <div id='chat-page'>
         <NavBar>aa</NavBar>
         <List>
-          <Item
-            thumb={require('../../assets/images/头像1.png')}
-          >
-            你好
-          </Item>
-          <Item
-            thumb={require('../../assets/images/头像1.png')}
-          >
-            你好2
-          </Item>
-          <Item
-            className='chat-me'
-            extra='我'
-          >
-            很好
-          </Item>
-          <Item
-            className='chat-me'
-            extra='我'
-          >
-            很好2
-          </Item>
+          {
+            msgs.map(msg => {
+
+              if(msg.to===meId) { // 他发给我的
+                return (
+                  <Item
+                    key={msg._id}
+                    thumb={targetHeader}
+                  >
+                    {msg.content}
+                  </Item>
+                )
+              } else { // 我发给他
+                return (
+                  <Item
+                    key={msg._id}
+                    className='chat-me'
+                    extra='我'
+                  >
+                    {msg.content}
+                  </Item>
+                )
+              }
+            })
+          }
         </List>
 
         <div className='am-tab-bar'>
@@ -62,6 +89,7 @@ class Chat extends Component {
               <span onClick={this.send}>发送</span>
             }
             onChange={val => this.setState({content: val})}
+            value={this.state.content}
           />
         </div>
       </div>
@@ -70,6 +98,6 @@ class Chat extends Component {
 }
 
 export default connect(
-  state => ({user: state.user}),
+  state => ({user: state.user, chat: state.chat}),
   {sendMsg}
 )(Chat)
