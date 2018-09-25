98834457ff2571d952473364bdbca7e285aaa9dc
zxfjd3g <258147149@qq.com>
2018-09-25 15:10:49 +0800

    complet

--- src/componnets/user-list/user-list.jsx
+++ src/componnets/user-list/user-list.jsx
@@ -2,9 +2,12 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {Card, WingBlank, WhiteSpace} from 'antd-mobile'
 import {withRouter} from 'react-router-dom'
+import QueueAnim from 'rc-queue-anim'
 
 const Header = Card.Header
 const Body = Card.Body
+
+
 
 /*
 用户列表组件
@@ -20,26 +23,28 @@ class UserList extends Component {
     const userList = this.props.userList.filter(user => user.header)
     return (
       <WingBlank style={{marginTop: 50, marginBottom: 50}}>
-        {
-          userList.map((user, index) => (
-            <div key={user._id}>
-              <WhiteSpace/>
-              <Card onClick={() => this.props.history.push(`/chat/${user._id}`)}>
-                <Header
-                  thumb={require(`../../assets/images/${user.header}.png`)}
-                  extra={user.username}
-                />
-                <Body>
-                {user.post ? <div>职位: {user.post}</div> : null}
-                {user.company ?<div>公司: {user.company}</div> : null}
-                {user.salary ? <div>月薪: {user.salary}</div> : null}
-                {user.info ?<div>描述: {user.info}</div> : null}
-                </Body>
-              </Card>
-            </div>
-          ))
-        }
-
+        {/*alpha left right top bottom scale scaleBig scaleX scaleY*/}
+        <QueueAnim type='bottom'>
+          {
+            userList.map((user, index) => (
+              <div key={user._id}>
+                <WhiteSpace/>
+                <Card onClick={() => this.props.history.push(`/chat/${user._id}`)}>
+                  <Header
+                    thumb={require(`../../assets/images/${user.header}.png`)}
+                    extra={user.username}
+                  />
+                  <Body>
+                  {user.post ? <div>职位: {user.post}</div> : null}
+                  {user.company ?<div>公司: {user.company}</div> : null}
+                  {user.salary ? <div>月薪: {user.salary}</div> : null}
+                  {user.info ?<div>描述: {user.info}</div> : null}
+                  </Body>
+                </Card>
+              </div>
+            ))
+          }
+        </QueueAnim>
       </WingBlank>
     )
   }
